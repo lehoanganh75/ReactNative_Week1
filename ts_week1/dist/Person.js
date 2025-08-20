abcf8d2a9e2d108cdcf54f4a5f7b6e282bc360af
lehoanganh <lehoanganh7524@gmail.com>
2025-08-20 07:47:50 +0700

    1. finish oop tsc

---
+++ ts_week1/dist/Person.js
@@ -0,0 +1,7 @@
+"use strict";
+let ten = "Le Hoang Anh";
+let age = 21;
+function displayInformation(ten, age) {
+    return `Xin chao ${ten} voi so tuoi la ${age} tuoi.`;
+}
+console.log(displayInformation(ten, age));
---
+++ ts_week1/src/Person.ts
@@ -0,0 +1,12 @@
+let ten: string = "Le Hoang Anh";
+let age = 21;
+
+function displayInformation(ten: string, age: number): string {
+    return `Xin chao ${ten} voi so tuoi la ${age} tuoi.`;
+}
+
+console.log(displayInformation(ten, age));
+
+
+
+
